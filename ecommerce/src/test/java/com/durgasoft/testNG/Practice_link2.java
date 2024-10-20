88119a9e026cfc3854789ea2ae992d3975ee96d0
kg1205 <ganeshkodimoju52@gmail.com>
2024-10-20 18:01:35 +0530

    Test Message

--- ecommerce/src/test/java/com/durgasoft/testNG/Practice_link2.java
+++ ecommerce/src/test/java/com/durgasoft/testNG/Practice_link2.java
@@ -4,9 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 
 public class Practice_link2 {
-  @Test
-  public void f() {
-  }
+  
   @BeforeTest
   public void beforeTest() {
   }
